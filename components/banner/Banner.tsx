a08a6014aabe063039a6fe7fad1d87805aac390f
createweb7 <createweb7@gmail.com>
2026-02-06 01:39:50 +0530

    Feature: Align button to left side in banner

--- components/banner/Banner.tsx
+++ components/banner/Banner.tsx
@@ -105,12 +105,14 @@ export default function Banner() {
 
                 <p className="text-base md:text-lg lg:text-xl opacity-90 mb-6">{slide.subtitle}</p>
 
-                <Link
-                  href={slide.buttonLink}
-                  className="bg-red-600 hover:bg-red-700 transition px-6 py-3 rounded-md text-white font-medium w-fit"
-                >
-                  {slide.buttonText}
-                </Link>
+                <div className="flex justify-start">
+                  <Link
+                    href={slide.buttonLink}
+                    className="bg-red-600 hover:bg-red-700 transition px-6 py-3 rounded-md text-white font-medium w-fit"
+                  >
+                    {slide.buttonText}
+                  </Link>
+                </div>
               </div>
             </div>
           </SwiperSlide>
